4be8a8b5594dc18198eac8f6e12b1e2dcda1df37
Jake Graham <jake.graham.95@gmail.com>
2021-03-07 19:32:09 -0500

    Finished Number Wizard UI

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] private int _maxGuess = 1000;
+    [SerializeField] private int _minGuess = 1;
+    [SerializeField] private TextMeshProUGUI _guessText;
+
+    private int _guess = 500;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    private void NextGuess()
+	{
+        _guess = Random.Range(_minGuess, _maxGuess+1);
+        _guessText.text = _guess.ToString();
+    }
+
+    public void OnPressHigher()
+	{
+        _minGuess = _guess +1;
+        NextGuess();
+
+    }
+
+	public void OnPressLower()
+	{
+        _maxGuess = _guess -1;
+        NextGuess();
+    }
+	
+}
